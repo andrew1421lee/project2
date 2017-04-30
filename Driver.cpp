5a8e0b9dc501f395010b5fa6cb8fa68d84731fc2
Anchu Lee <andrew1421lee@gmail.com>
2017-04-30 18:35:16 -0400

    it doesnt work

--- Driver.cpp
+++ Driver.cpp
@@ -8,7 +8,7 @@ struct BLOCK
     char valid;
     unsigned tag;
     unsigned last;
-    //unsigned long temp; //Lol lets see
+    //char hot; //Lol lets see
 };
 
 struct NODE
@@ -19,6 +19,7 @@ struct NODE
 
 //Global Variables
 std::string outfile;
+std::string infilename;
 std::string dummy;
 
 //Forward Declarations
@@ -28,20 +29,31 @@ bool FullAssCache(std::ifstream &infile);
 bool NoAssCache(std::ifstream &infile);
 bool PreAssCache(std::ifstream &infile);
 bool MissAssCache(std::ifstream &infile);
+bool OptimalCache(std::ifstream &infile);
 std::string GetHexString(unsigned long hex);
 void BeKindRewind(std::ifstream &infile);
 
-int main(int argc, char** argv)
+int main(int argc, char* argv[])
 {
     //Invalid input check
-    if(argc < 3 || argc > 3)
+    if(argc < 3 || argc > 4)
     {
         std::cout << "Usage: " << argv[0] << " [input] [output]" << std::endl;
         return 0;
     }
 
+    bool unlocked = false;
+    if(argc == 4)
+    {
+        if(std::string(argv[3]) == "-c")
+        {
+            LogPrint(INFO, "UNLOCKED");
+            unlocked = true;
+        }
+    }
     //save output file
     outfile = argv[2];
+    infilename = argv[1];
     //Load input file
     std::ifstream infile(argv[1]);
     //If file exists and opened
@@ -54,8 +66,11 @@ int main(int argc, char** argv)
         if(!NoAssCache(infile)) LogPrint(ERROR, "Failed Write Miss Associative Cache");
         if(!PreAssCache(infile)) LogPrint(ERROR, "Failed Prefetch Associative Cache");
         if(!MissAssCache(infile)) LogPrint(ERROR, "Failed Prefetch Associative Cache");
-
-
+        if(unlocked)
+        {
+            LogPrint(INFO, "Super secret replacement unlocked!");
+            OptimalCache(infile);
+        }
     }
 }
 
@@ -259,7 +274,7 @@ bool FullAssCache(std::ifstream &infile)
         if(i != 0)
         {
             returnstring << "\n";
-            
+            ////////////////////////////////////////////////////
             bst = new NODE[1023];
             for(int j = 0; j < 1023; j++)
             {
@@ -275,6 +290,7 @@ bool FullAssCache(std::ifstream &infile)
                 bst[j].index = -1;
                 //LogPrint(DEBUG, "DOES THIS WORK?" + std::to_string(j));
             }
+            
         }
 
         //important variables
@@ -290,7 +306,7 @@ bool FullAssCache(std::ifstream &infile)
             cache[j].valid = 0;
             cache[j].tag = 0;
             cache[j].last = 0;
-            //cache[j].temp = 0;
+            //cache[j].hot = 0; ///////////////////
         }
 
         //Variables for reading
@@ -326,6 +342,7 @@ bool FullAssCache(std::ifstream &infile)
                         //cache[j].temp++;
                         //Find which item in the BST corresponds to the cache
                         
+                        //cache[j].hot = 1;
                         cache[j].last = accesses;
 
                         float bst_index;
@@ -362,7 +379,6 @@ bool FullAssCache(std::ifstream &infile)
                             //LogPrint(DEBUG, "Fixing parents on access");
                         }
                         //LogPrint(DEBUG, "OK fixed parents");
-
                     }
                     hits++;
                     found = true;
@@ -870,6 +886,156 @@ bool MissAssCache(std::ifstream &infile)
            
             }
 
+            accesses++;
+        }
+
+        //Add values to string
+        returnstring << std::to_string(hits) << "," << std::to_string(accesses) << ";";
+
+        //Be kind, rewind!
+        BeKindRewind(infile);
+        //Free allocated memory
+        delete [] cache;
+    }
+    //Write to file
+    WriteFile(APPEND, outfile, "\n" + returnstring.str());
+    return true;
+}
+
+bool OptimalCache(std::ifstream &infile)
+{
+    //Magic numbers!
+    int asses[4] = {2, 4, 8, 16};
+    int shifts[4] = {8, 7, 6, 5};
+    int cachesize = 16384;
+    std::stringstream returnstring;
+
+    for(int i = 0; i < 4; i++)
+    {
+        if(i != 0)
+        {
+            returnstring << " ";
+        }
+
+        unsigned long hits = 0;
+        unsigned long accesses = 0;
+        
+        BLOCK** cache = new BLOCK*[cachesize / (32 * asses[i])];
+
+        //Loops number of sets
+        for(int j = 0; j < (cachesize / (32 * asses[i])); j++)
+        {
+            cache[j] = new BLOCK[asses[i]];
+            for(int k = 0; k < asses[i]; k++)
+            {
+                cache[j][k].valid = 0;
+                cache[j][k].tag = 0;
+                cache[j][k].last = 0;
+            }
+            //LogPrint(INFO, std::to_string(asses[i]) + " - " + std::to_string(j));
+        }
+        //Variables for reading
+        std::string instr;
+        unsigned long addr;
+        //unsigned long time;
+        
+        std::ifstream the_future(infilename);
+
+        //Reading loop
+        while(infile >> instr >> std::hex >> addr)
+        {
+            //LOL offset
+            addr >>= 5;
+            //Index is found by Block Address modulo sets in cache
+            unsigned cindex = addr % (cachesize / (32 * asses[i]));
+            //Tag is the rest of the bits
+            unsigned ctag = addr >> shifts[i];
+
+            bool found = false;
+
+            //Loop through each block in the set
+            //Check for hit or empty spot
+            for(int j = 0; j < asses[i]; j++)
+            {
+                //valid block and correct tag
+                if(cache[cindex][j].valid && cache[cindex][j].tag == ctag)
+                {
+                    cache[cindex][j].last = accesses; //update time
+                    hits++; //inc hits
+                    found = true;
+                    break; // stop looking
+                }
+                //Empty spot
+                else if(!cache[cindex][j].valid)
+                {
+                    //empty spot, add to cache
+                    cache[cindex][j].valid = 1;
+                    cache[cindex][j].tag = ctag;
+                    cache[cindex][j].last = accesses;
+                    found = true;
+                    break;
+                }
+            }
+            //Kick OPT out
+            if(!found)
+            {
+                unsigned opt = 0;
+                //loop entire file
+                unsigned counter = 0;
+                unsigned* counters = new unsigned[asses[i]];
+                for(int p = 0; p < asses[i]; p++)
+                {
+                    counters[p] = 0;
+                }
+                unsigned o_instr;
+                unsigned o_addr;
+
+                while(the_future >> o_instr >> std::hex >> o_addr)
+                {
+                    o_addr >>= 5;
+                    unsigned o_index = o_addr % (cachesize/ (32 * asses[i]));
+                    unsigned o_tag = o_addr >> shifts[i];
+                
+                    for(int j = 0; j < asses[i]; j++)
+                    {
+                        if(cache[cindex][j].tag == o_tag)
+                        {
+                            //next occurence
+                            if(!(counters[j] < counter))
+                            {
+                                counters[j] = counter;
+                            }
+                        }
+                        counter++;
+                    }
+                }
+                    
+                //move it back lol
+                the_future.clear();
+                //infile.seekg(0, std::ios::beg);
+                the_future.seekg(accesses - 1, std::ios::beg);
+
+                //infile.seekg(accesses, std::ios::beg);
+
+//                LogPrint(DEBUG, std::to_string(accesses));
+                
+                //unsigned largest = 0;
+                for(int p = 0; p < asses[i]; p++)
+                {
+                    if(counters[p] > counters[opt])
+                    {
+                        opt = p;
+                    }
+                }
+
+                cache[cindex][opt].tag = ctag;
+                cache[cindex][opt].last = accesses;
+            }
+            else
+            {
+                the_future.seekg(1, std::ios::cur);
+            }
+            
             accesses++;
         }
 
